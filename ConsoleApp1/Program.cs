b0713663239790a9d0cade30bc0981d070a51d79
DESKTOP-VLTCHR6\Roy Developer <rjisraelcubillan55@gmail.com>
2025-03-25 18:36:43 +0800

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+class RePointSolutionsQuiz {
+    static void Main() {
+        Console.Write("Input your data here: ");
+        string input = Console.ReadLine();
+        int number = ParseTheNumberFromString(input);
+        Console.WriteLine("The extracted number is: " + number);
+    }
+    static int ParseTheNumberFromString(string input) {
+        StringBuilder numberBuilder = new StringBuilder();
+         foreach (char valuesainput in input){
+            if (char.IsDigit(valuesainput)){
+                numberBuilder.Append(valuesainput);
+            }
+        }
+        return numberBuilder.Length > 0 ? int.Parse(numberBuilder.ToString()) : 0;
+    }
+}
